b1933c262a71d0f0b9d1dba62dd0e280a6ae9db1
goburhan <52301010+goburhan@users.noreply.github.com>
2021-06-03 14:44:42 +0300

    Add files via upload

---
+++ campaign.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.17;
+
+contract Campaign {
+    
+        struct Request{
+            string description;
+            uint value; 
+            address recipient;
+            bool complete; 
+            uint approvalCount; 
+            mapping(address=>bool) approvals;
+        }
+        Request[] public requests;
+        
+        address public manager; 
+        uint public minimumContribution;
+      
+        mapping (address=>bool) approvers;
+        uint public approversCount;
+        
+        modifier restricted(){
+            require(msg.sender == manager);
+            _;
+        }
+        function createRequest(string description , uint value , address recipient)public restricted {
+            require(approvers[msg.sender]);
+            Request memory newRequest = Request({
+               description : description ,
+               value : value,
+               recipient : recipient,
+               complete : false,
+               approvalCount:0
+            });
+            requests.push(newRequest);
+        }
+        
+        function Campaign(uint minimum) public{
+            manager = msg.sender;
+            minimumContribution = minimum;
+        }
+        
+        function contribute()public payable {
+            require (msg.value> minimumContribution);
+            approvers[msg.sender] = true ;
+            approversCount++;
+        }
+        
+        function approveRequest(uint index) public{
+            require(approvers[msg.sender]);
+            require(!requests[index].approvals[msg.sender]);
+            
+        requests[index].approvals[msg.sender]= true;    
+        requests[index].approvalCount++;
+        }
+        
+        function finalizeRequest(uint index) public restricted {
+            Request storage request = requests[index]; 
+            require(!request.complete);
+            
+            request.recipient.transfer(request.value);
+            request.complete = true; 
+        }
+        
+        
+}
